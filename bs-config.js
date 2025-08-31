bb7b1dc055edcd38776c8042fb759e4166dddca8
CookBreh <simon05@tutamail.com>
2025-08-31 22:53:33 +0200

    Fix browser-sync configuration and restart server

--- bs-config.js
+++ bs-config.js
@@ -1,16 +1,17 @@
 export default {
-  server: "templates/dist",
-  files: "templates/dist/**/*",
-  port: 3000,
-  middleware: [
-    {
-      route: "",
-      handle: function (req, res, next) {
+  server: {
+    baseDir: "templates/dist",
+    middleware: [
+      function (req, res, next) {
         // Remove any CSP headers that browser-sync might set
         res.removeHeader('Content-Security-Policy');
         res.removeHeader('Content-Security-Policy-Report-Only');
         next();
       }
-    }
-  ]
+    ]
+  },
+  files: "templates/dist/**/*",
+  port: 3000,
+  open: false,
+  notify: false
 };
